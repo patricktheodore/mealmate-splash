37baa176807c195bcdb668e2c25798b2f97ebf73
patricktheodore <patricktheodoresara@gmail.com>
2025-05-30 17:58:09 +0800

    git init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		formats: ['image/avif', 'image/webp'],
+	},
+	compress: true,
+	poweredByHeader: false,
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-Frame-Options',
+						value: 'DENY',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+				],
+			},
+		];
+	},
 };
 
-export default nextConfig;
+module.exports = nextConfig;
